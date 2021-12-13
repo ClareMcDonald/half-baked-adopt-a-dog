b4a5e199f0e2c560728c8610f9ce1b7c3c91f75d
ClareMcDonald <clare.s.mcdonald@gmail.com>
2021-12-13 12:43:10 -0800

    initial work

--- detail/detail.js
+++ detail/detail.js
@@ -4,6 +4,16 @@ import { renderDogDetail } from '../render-utils.js';
 const dogDetailContainer = document.getElementById('dog-detail-container');
 
 // on load
+window.addEventListener('load', async() => {
 // get the id from URL
+    const params = new URLSearchParams(window.location.search);
+    const id = params.get('id');
+
 // use the id to fetch the dog
+    const dog = await getDog(id);
+
 // render and append this dog's details to the container
+    renderDogDetail(dog);
+    dogDetailContainer.append(dog);
+});
+
