cf20fd744f3d375c2a5e1ec967ee06303d248265
ClareMcDonald <clare.s.mcdonald@gmail.com>
2021-12-13 17:27:18 -0800

    test passes

--- test/example.test.js
+++ test/example.test.js
@@ -1,31 +1,34 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
 
-import { renderDogCard } from '../render-utils';
+import { renderDogCard } from '../render-utils.js';
 
 const test = QUnit.test;
 
-test('renderDogCard', (expect) => {
+test('renderDogCard should take in a dog and return an appropriate DOM element', (expect) => {
     //Arrange
     // Set up your arguments and expectations
     const testDog = {
-        name: 'Finicky',
-        breed: 'dachshund',
-        id: 1
+        name: 'fluffy',
+        breed: 'red heeler',
+        id: 3
     };
 
-    const expected = '<a href="./detail/?id=1"><div class="dog-card"><p>Finicky</p><img src="./assets/dachshund.jpeg"></div></a>';
+    const expected = '<a href=\"./detail/?id=3\"><div class=\"dog-card\"><p>fluffy</p><img src=\"./assets/red heeler.jpeg\"></div></a>';
+
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = renderDogCard(testDog).outerHTML;
-
+    const actual = renderDogCard(testDog);
+    
+    // console.log(actual);
+    
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual.outerHTML, expected);
+    expect.equal(actual.outerHTML, expected, 'the string representation of the actual element should be the same as described in the test');
 });
 
 
-test('renderDogDetail', (expect) => {
+/* test('renderDogDetail', (expect) => {
     //Arrange
     // Set up your arguments and expectations
 
@@ -37,4 +40,4 @@ test('renderDogDetail', (expect) => {
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
-});
+}); */
